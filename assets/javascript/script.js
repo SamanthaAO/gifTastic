c106245c0329fbb8ec7adfa08cef6b22dc95efe5
SamanthaAO <samanthaaorcutt@gmail.com>
2019-05-03 21:40:38 -0700

    made favorites list save to local storage so can be viewed after refresh

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,7 +4,9 @@ var searchterm;
 var topics = ["Adventure Time", "Spongebob", "Fairly OddParents", "Powerpuff Girls", "Family Guy", "South Park", "The Simpsons", "Tom and Jerry", "Teenage Mutant Ninja Turtles", "Avatar: The Last Airbender"];
 var results = [];
 var favs = [];
+var storedfavs = [];
 
+//checks to see if a card is already in favorites array
 function isAlreadyFavorite(id){
     var match = favs.filter(function(fav){return id === fav.id});
     //if already exists the obj will already be in the favs array therefore match.length > 0
@@ -20,6 +22,7 @@ function createButtons() {
     $("#buttonArea").html(button);
 }
 
+//calls the api with ajax then sets results and triggers createGifCard
 function apiCall(queryUrl){
     $.ajax({
         url: queryUrl,
@@ -27,19 +30,20 @@ function apiCall(queryUrl){
     })
         .then(function (response) {
             results = response.data;
-            //window.results = results;
-            console.log(results);
-
             
             createGifCard(true);
         })
 }
 
+//creates gifCards for gifs displayed from api and favorites
 function createGifCard(isApiCall) {
     var insert = "";
-    var thisArray = (isApiCall) ? results : favs;
+    //looks to see which set of data to use to create cards with
+    var thisArray = (isApiCall) ? results : storedfavs;
 
+    //creates one card for each 
     thisArray.forEach(function (result, i) {
+        //gets rid of favorite button if card already in favorites
         var btnHtml = (!isAlreadyFavorite(result.id)) ? `<button type="button" class="btn btn-outline-dark favorite" id="${i}">Add to Favorites</button>` : '';
         insert +=
             `<div class="col-lg-4 col-md-6" id="card${i}">
@@ -56,12 +60,16 @@ function createGifCard(isApiCall) {
 
 
     })
+
+    //displays the cards on the page
     $("#gifArea").html(insert);
-    
+
+    //displays title for gifs
     $("#titleArea").html("<div class='jumbotron jumbotron-fluid p-0 text-center'><h1 class='display-4'>GIFs</h1></div>");
     
 }
 
+//makes sure that theere is an input when enter or create new button is clicked
 function contentPresent(newButton){
     if (newButton) {
         topics.push(newButton);
@@ -72,18 +80,16 @@ function contentPresent(newButton){
 }
 
 
-
-
-
-
 $(document).ready(function () {
 
+    //creates initial buttons from array
     createButtons();
 
     
     //takes input and makes it into a gif creating button
     $("#newButtonMaker").on("click", function NewButtonMakerClick() {
-        var newButton = $("#buttonInput").val();
+        var newButton = $("#buttonInput").val().trim();
+
         contentPresent(newButton);
         
     });
@@ -91,44 +97,35 @@ $(document).ready(function () {
     //enter can also be used to submit
     $("#buttonInput").on("keypress", function(e){
         if(e.which == 13){
-            var newButton = $("#buttonInput").val();
+            var newButton = $("#buttonInput").val().trim();
             
             contentPresent(newButton);
         }
     });
 
 
-    //on click of gif creating button call API and create cards with gifs within
+    //on click of gif creating button (call API and create cards with gifs within from function chan starting with apiCall)
     $("#buttonArea").on("click", ".gifButton", function () {
+        //sets everything back to start
         $("#gifArea").empty();
         $("#jokeDiv").empty();
         $("#loadMore").empty();
-        num = 10;
 
+        //takes name from button and makes it search term
         searchTerm = $(this).attr("id");
-        //window.searchTerm = searchTerm;
-        console.log(searchTerm)
-        var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=TBxfYQsStw6XpZp4RtzXaPa38rSfVCyY&limit=" + num;
-
-        apiCall(queryUrl);
         
 
-            $("#loadMore").append("<button type='button' class='btn btn-light btn-outline-dark btn-lg btn-block mt-2 ' id ='loadMore'>Load more gifs</button>");
+        var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=TBxfYQsStw6XpZp4RtzXaPa38rSfVCyY&limit=10";
 
-        // var jokeURL = "https://geek-jokes.sameerkumar.website/api";
-
-        // $.ajax({
-        //     url: jokeURL,
-        //     method: "GET"
-        // })
-        //     .then(function (joke) {
-        //         var jokeDiv = $("<div>").text(joke);
-        //         jokeDiv.attr("id", "jokeDiv");
-        //         $("#buttonArea").prepend(jokeDiv);
-        //     })
+        //gets information from api
+        apiCall(queryUrl);
+        
+        //creates load more button
+        $("#loadMore").append("<button type='button' class='btn btn-light btn-outline-dark btn-lg btn-block mt-2 ' id ='loadMore'>Load more gifs</button>");
 
     });
 
+    //changes state of image from still to animate
     $("#gifArea").on("click", ".gif", function () {
 
         var state = $(this).attr("state");
@@ -143,32 +140,49 @@ $(document).ready(function () {
         }
     });
 
+    //functionality of load more button increases num so that more gifs are loaded
     $("#loadMore").on("click", "#loadMore", function(){
 
-        console.log(searchTerm);
         num = num +10;
         var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=TBxfYQsStw6XpZp4RtzXaPa38rSfVCyY&limit=" + num;
         apiCall(queryUrl);
 
     })
 
-
+    //favorite button on gifCard functionality
     $("#gifArea").on("click", ".favorite", function () {
-        
+        //gets specific card id in api array
         thisGif = results[this.id];
 
+        //if it is not already in favorites  
         if (!isAlreadyFavorite(thisGif.id)) 
         {
+            //add it to favorites
             favs.push(results[this.id]);
+            
+            //remove the add to favorites button from this card so then must re call the create gif card to now how it shown without the favorite button
             createGifCard(true);
         }
+
+        //updates local storage with favs list
+        localStorage.setItem("favs", JSON.stringify(favs));
 
         
     });
 
+    //view favorites button sends you to favorite gifs screen to fiew favorite gifs
     $("#favorites").on("click", function () {
+        
+        //gets rid of load more button on the bottom
         $("#loadMore").empty();
+
+        //pulls fav list from local storage
+        storedfavs = JSON.parse(localStorage.getItem("favs"));
+
+        //uses fav array to createGifCard
         createGifCard(false);
+
+        //changes title area to say favorite gifs
         $("#titleArea").html("<div class='jumbotron jumbotron-fluid p-0 text-center'><h1 class='display-4'>Favorite GIFs</h1></div>");
     })
 
